70d9373a0962d9aaa391de3bb6049de8c07c9093
JaoPred0 <jhonpitere163@gmail.com>
2025-10-26 00:19:07 -0400

    segundo projeto

--- src/data/templatesData.js
+++ src/data/templatesData.js
@@ -8,5 +8,12 @@ export const templatesData = [
     image: '/capa/1.png',
     site: 'https://amor-eterno-five.vercel.app'
   },
-  
+  {
+    id: 2,
+    title: 'Nossa História',
+    description: 'Um espaço dedicado para celebrar cada momento, cada sorriso e cada memória que construímos juntos',
+    price: 'R$ 50,00',
+    image: '/capa/2.png',
+    site: 'https://nossa-historia-one.vercel.app'
+  },
 ]
